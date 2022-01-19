7b52c1f4825dee7c4a2c50411cef63bf46823f68
Sudhir <srplilu0@gmail.com>
2022-01-19 11:53:56 -0800

    Added models to query the metadata  for all the tables and views from INFORMATION_SCHEMA

--- models.py
+++ models.py
@@ -39,3 +39,50 @@ class Board(Base):
     zip = Column(Integer)
     created_date = Column(TIMESTAMP(timezone=True), server_default=func.timestamp())
     updated_date = Column(TIMESTAMP(timezone=True), server_default=func.timestamp())
+
+
+class Tables(Base):
+    __tablename__ = "TABLES"
+
+    # row_id = Column(String, primary_key=True, server_default=text("uuid.uuid4()"))
+    table_catalog = Column(String)
+    table_schema = Column(String)
+    table_name = Column(String)
+    table_owner = Column(String)
+    table_type = Column(String)
+    is_transient = Column(String)
+    clustering_key = Column(String)
+    row_count = Column(Numeric(38, 0, 0, False))
+    bytes = Column(Numeric(38, 0, 0, False))
+    retention_time = Column(Numeric(9, 0, 0, False))
+    self_referencing_column_name = Column(String)
+    reference_generation = Column(String)
+    user_defined_type_catalog = Column(String)
+    user_defined_type_schema = Column(String)
+    user_defined_type_name = Column(String)
+    is_insertable_into = Column(String)
+    is_typed = Column(String)
+    commit_action = Column(String)
+    created = Column(TIMESTAMP(timezone=True))
+    last_altered = Column(TIMESTAMP(timezone=True))
+    auto_clustering_on = Column(String)
+    comment = Column(String)
+    PrimaryKeyConstraint(table_schema, table_name, name="tables_pk")
+
+
+class Views(Base):
+    __tablename__ = "VIEWS"
+
+    table_catalog = Column(String)
+    table_schema = Column(String)
+    table_name = Column(String)
+    table_owner = Column(String)
+    view_definition = Column(String)
+    check_option = Column(String)
+    is_updatable = Column(String)
+    insertable_into = Column(String)
+    is_secure = Column(String)
+    created = Column(TIMESTAMP(timezone=True))
+    last_altered = Column(TIMESTAMP(timezone=True))
+    comment = Column(String)
+    PrimaryKeyConstraint(table_schema, table_name, name="views_pk")

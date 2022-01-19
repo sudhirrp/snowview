9d3ce96a18782f99beeae2b2cb56699b18342307
Sudhir <srplilu0@gmail.com>
2022-01-19 12:02:49 -0800

    Attached the Queries and Mutations to the graphQl app

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 
 from custom_graphql import CustomGraphQLApp
 from db_conf import db_session
+from mutations import PostMutations
+from queries import Query
 
 db = db_session.session_factory()
 
 app = FastAPI()
 
 
-app.add_route("/graphql", CustomGraphQLApp(schema=graphene.Schema(query=None, mutation=None)))
+app.add_route("/graphql", CustomGraphQLApp(schema=graphene.Schema(query=Query, mutation=PostMutations)))

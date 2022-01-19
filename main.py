c3fde675e6d6bb5cef529bd301745e6100f33bc9
Sudhir <srplilu0@gmail.com>
2022-01-19 11:37:57 -0800

    Added GraphQl capability to the project

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import graphene
 from fastapi import FastAPI
 
+from custom_graphql import CustomGraphQLApp
 from db_conf import db_session
 
 db = db_session.session_factory()
 
 app = FastAPI()
+
+
+app.add_route("/graphql", CustomGraphQLApp(schema=graphene.Schema(query=None, mutation=None)))

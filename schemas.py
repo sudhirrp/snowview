0118a6c4b36b76ac67b37b70c11a6e896a355e9e
Sudhirrp <sudhir.pradhan@rightrev.com>
2022-01-19 11:54:21 -0800

    Added respective schemas to query the metadata  for all the tables and views from INFORMATION_SCHEMA

--- schemas.py
+++ schemas.py
@@ -26,3 +26,53 @@ class BoardSchema(BaseModel):
 class BoardModel(SQLAlchemyObjectType):
     class Meta:
         model = Board
+
+
+class ViewsSchema(BaseModel):
+    table_catalog = str
+    table_schema = str
+    table_name = str
+    table_owner = str
+    view_definition = str
+    check_option = str
+    is_updatable = str
+    insertable_into = str
+    is_secure = str
+    created = str
+    last_altered = str
+    comment = str
+
+
+class ViewsModel(SQLAlchemyObjectType):
+    class Meta:
+        model = Views
+
+
+class TablesSchema(BaseModel):
+    table_catalog = str
+    table_schema = str
+    table_name = str
+    table_owner = str
+    table_type = str
+    is_transient = str
+    clustering_key = str
+    row_count = str
+    bytes = str
+    retention_time = str
+    self_referencing_column_name = str
+    reference_generation = str
+    user_defined_type_catalog = str
+    user_defined_type_schema = str
+    user_defined_type_name = str
+    is_insertable_into = str
+    is_typed = str
+    commit_action = str
+    created = str
+    last_altered = str
+    auto_clustering_on = str
+    comment = str
+
+
+class TablesModel(SQLAlchemyObjectType):
+    class Meta:
+        model = Tables

f2b4d2424ac9ef24db4b02ea99a2d6f28b7927f1
Sudhirrp <sudhir.pradhan@rightrev.com>
2022-01-20 14:02:44 -0800

    Added project setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="puflin",
+    version="0.1",
+    author="rightrev",
+    author_email="engg@rightrev.com",
+    description="RightRev GraphQL service to query Snowflake warehouse.",
+    packages=[],
+    package_dir={'puflin': '.'},
+    install_requires=[
+                      ]
+)
